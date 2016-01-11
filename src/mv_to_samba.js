0ea210f4875af4efc4192b488320957f1b05cfa7
Russell Hueske <hueske.russ690@gmail.com>
2016-01-11 17:53:56 -0600

    ugh

--- src/mv_to_samba.js
+++ src/mv_to_samba.js
@@ -1,11 +1,17 @@
 import * as mv from 'mv';
-
-export function move(obj) {
+var fs = require('fs');
+export default function move(obj) {
   const dir = `${obj.path}/${obj.name}`;
 
   var path_arr = obj.path.split('/');
   var label = path_arr[path_arr.length - 1];
-  mv(dir, `/root/deluge/${label}`, (err) => {
+  var newPath = `/root/deluge/${label}`;
+  fs.writeSync('/root/deluge_log.log', JSON.stringify({
+    label: label,
+    original: dir,
+    newPath: newPath
+  }, null, 2));
+  mv(dir, newPath, (err) => {
     if (err) throw err;
   });
 }
